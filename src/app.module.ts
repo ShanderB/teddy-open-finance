e6cbb0ec83007827935c17df2962c4f00b58869d
ShanderB <s.hander8910@hotmail.com>
2024-08-24 13:17:18 -0300

    Criado módulos de autenticação, de usuário e principal.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getOrmConfig } from './auth/constants';
 
 @Module({
-	imports: [],
+	imports: [
+		AuthModule,
+		UsersModule,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: () => getOrmConfig()
+		})
+	],
 	controllers: [],
 	providers: []
 })
---
+++ src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+	imports: [
+		TypeOrmModule.forFeature([User]),
+		ConfigModule.forRoot({
+			isGlobal: true
+		})
+	],
+	providers: [UsersService],
+	controllers: [UsersController],
+	exports: [UsersService]
+})
+export class UsersModule {}
